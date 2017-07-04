fb6a1e454930810d790481f88e0167af46e2e7ec
Cho Bong Ki <cho1036216@naver.com>
2017-07-04 21:18:41 +0900

    9:18

--- src/com/hanbit/oop/domain/MemberBean.java
+++ src/com/hanbit/oop/domain/MemberBean.java
@@ -1,4 +1,5 @@
 package com.hanbit.oop.domain;
+import com.hanbit.oop.domain.MemberBean;
 
 public class MemberBean {
 	private String ssn, id, pass, name ;
@@ -36,8 +37,10 @@ public class MemberBean {
 
 		return ssn;
 	}
+
+	@Override
 	public String toString() {
-		return "";
-		//return "환영합니다" + name + "\t" + gender + "\t" + age;
+		return "회원 [주민번호=" + ssn + ", id=" + id + ", pass=" + pass + ", name=" + name + "]";
 	}
+	
 }
